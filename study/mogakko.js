72b0acf3ca94f4d406ebf0327f6cafe7c2db8ec3
uoayop <uoayop@kakao.com>
2021-05-26 13:38:49 +0900

    [ADD] project layout added

---
+++ study/mogakko.js
@@ -0,0 +1,14 @@
+function mo_btn(){
+    var mo_txt = document.getElementById("mo_txt");
+    mo_txt.innerText = '모두 모여!'
+}
+
+function gak_btn(){
+    var gak_btn = document.getElementById("gak_txt");
+    gak_btn.innerText = '각이다.'
+}
+
+function ko_btn(){
+    var ko_btn = document.getElementById("ko_txt");
+    ko_btn.innerText = '코딩 각이다!'
+}
