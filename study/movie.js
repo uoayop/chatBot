e9e7456a7ba14f7a282ac7f90d2f60ac0b6dbbcb
uoayop <uoayop@kakao.com>
2021-06-03 14:06:47 +0900

    [ADD] movie slot stuy

---
+++ study/movie.js
@@ -0,0 +1,39 @@
+var title = document.getElementById('title');
+var year = document.getElementById('year');
+var still = document.getElementById('still');
+var quotes = document.getElementById('quotes');
+
+function toystory(){
+    title.innerText = '토이스토리';
+    year.innerText = '(2019)';
+    still.src = './img/toystory.jpeg';
+    quotes.innerText = '"무한한 공간 저 너머로 🚀"';
+}
+
+function soul(){
+    title.innerText = '소울';
+    year.innerText = '(2020)';
+    still.src = './img/soul.jpeg';
+    quotes.innerText = '"나는 내 삶의 모든 시간을 살아갈거야 🍀"';
+}
+
+function lalaland(){
+    title.innerText = '라라랜드';
+    year.innerText = '(2016)';
+    still.src = './img/lalaland.jpeg';
+    quotes.innerText = '"People love what other people are passionate about. ✨"';
+}
+
+function abouttime(){
+    title.innerText = '어바웃타임';
+    year.innerText = '(2013)';
+    still.src = './img/abouttime.jpeg';
+    quotes.innerText = '" Live life as if there were no second chance. 🕰"';
+}
+
+function singstreet(){
+    title.innerText = '싱스트릿';
+    year.innerText = '(2016)';
+    still.src = './img/singstreet.jpeg';
+    quotes.innerText = '"절대 적당히 해선 안 돼. 알아들었어? 🥊"';
+}
