d5858216319abdf3da44f9e9c890d1a4c828f4c6
uoayop <uoayop@kakao.com>
2021-06-10 15:04:43 +0900

    [ADD] animation.css / Typewriter CDN installed

--- animation.js
+++ animation.js
@@ -1,115 +1,103 @@
+function DanceAnimate() {
+  setTimeout(() => {
+    light();
+    var target = document.getElementById("bot-image");
 
-function DanceAnimate(){
-    setTimeout(() => {
-        light();
-        var target = document.getElementById('bot-image');
-
-        try { 
-            target.classList.add("dance");
-        } catch (error) {
-            
-        } finally {
-            setTimeout(() => {
-                target.className = "";
-                setBotMessage('어때?! 멋져?');
-            }, 2500);
-        }
-    }, 1300);
-    
-}
-
-function light(){
-    var background = document.getElementsByTagName('body')[0];
-    var lights = ['#ffe6e6', '#ffeecc', '#ffffcc', '#d9ffb3', '#cceeff',
-                '#b3c6ff', '#e0ccff', '#ffe6ff'];
-    var lightingBool = false;
-    var index = 0;
-
-    if (lightingBool == false){ // 타이핑이 진행되지 않았다면 
-        lightingBool = true; 
-        var liInt = setInterval(lighting,260); // 반복동작 
-    } 
-
-    function lighting(){ 
-        if (index < lights.length){ // 타이핑될 텍스트 길이만큼 반복 
-            // 한글자씩 이어준다. 
-            background.style.backgroundColor =  lights[index];
-            index++; 
-        } else{ 
-            clearInterval(liInt); //끝나면 반복종료 
-            background.style.backgroundColor = '#FFF';
-        }    
-    }  
-}
-
-function NodAnimate(){
-    var target = document.getElementById('bot-image');
-    try { 
-        target.classList.add("dontknow");
+    try {
+      target.classList.add("dance");
     } catch (error) {
-        
     } finally {
-        setTimeout(() => {
-            target.className = "";
-        }, 600);
+      setTimeout(() => {
+        target.className = "";
+        typing("어때?! 멋져?");
+      }, 2500);
     }
+  }, 1300);
 }
 
-function MoveAnimate(){
+function light() {
+  var background = document.getElementsByTagName("body")[0];
+  var lights = [
+    "#ffe6e6",
+    "#ffeecc",
+    "#ffffcc",
+    "#d9ffb3",
+    "#cceeff",
+    "#b3c6ff",
+    "#e0ccff",
+    "#ffe6ff",
+  ];
+  var lightingBool = false;
+  var index = 0;
+
+  if (lightingBool == false) {
+    // 타이핑이 진행되지 않았다면
+    lightingBool = true;
+    var liInt = setInterval(lighting, 260); // 반복동작
+  }
+
+  function lighting() {
+    if (index < lights.length) {
+      // 타이핑될 텍스트 길이만큼 반복
+      // 한글자씩 이어준다.
+      background.style.backgroundColor = lights[index];
+      index++;
+    } else {
+      clearInterval(liInt); //끝나면 반복종료
+      background.style.backgroundColor = "#FFF";
+    }
+  }
+}
+
+function Animate(class_name) {
+  var target = document.getElementById("bot-image");
+  try {
+    target.classList.add(class_name);
+  } catch (error) {
+  } finally {
     setTimeout(() => {
-        var target = document.getElementById('bot-image');
-        try { 
-            target.classList.add("move");
-        } catch (error) {
-            
-        } finally {
-            setTimeout(() => {
-                target.style.marginLeft = '2000px';
-                target.className = "";
-            }, 1000);
-        }
-    }, 500);
+      target.className = "";
+    }, 600);
+  }
 }
 
-function BackAnimate(){
-    setTimeout(() => {
-        var target = document.getElementById('bot-image');
-        try { 
-            target.classList.add("back");
-        } catch (error) {
-            
-        } finally {
-            setTimeout(() => {
-                target.style.marginLeft = '0px';
-                target.className = "";
-            }, 1000);
-        }
-    }, 500);
+function MoveAnimate() {
+  setTimeout(() => {
+    var target = document.getElementById("bot-image");
+    try {
+      target.classList.add("goaway");
+    } catch (error) {
+    } finally {
+      setTimeout(() => {
+        target.style.opacity = "0";
+        target.className = "";
+      }, 1000);
+    }
+  }, 500);
 }
 
-function typing(){
-    var bot_message = document.getElementById('bot-message');
-    
-    var typingBool = false;
-    var index=0; 
-    var target = bot_message.innerText.split(""); // 타이핑될 텍스트를 가져와서 한글자씩 자른다.
-    bot_message.innerText = '';
-
-    var temp = '';
-
-    if (typingBool == false){ // 타이핑이 진행되지 않았다면 
-        typingBool = true; 
-        var tyInt = setInterval(typing,100); // 반복동작 
-    } 
-
-    function typing(){ 
-        if (index < target.length){ // 타이핑될 텍스트 길이만큼 반복 
-            temp += target[index]; // 한글자씩 이어준다. 
-            bot_message.innerText = temp;
-            index++; 
-        } else{ 
-            clearInterval(tyInt); //끝나면 반복종료 
-        }    
-    }  
+function BackAnimate() {
+  setTimeout(() => {
+    var target = document.getElementById("bot-image");
+    target.style.opacity = "1";
+    try {
+      target.classList.add("comeback");
+    } catch (error) {
+    } finally {
+      setTimeout(() => {
+        target.className = "";
+        //   target.style.marginLeft = "0px";
+      }, 1100);
+    }
+  }, 500);
 }
 
+function typing(str) {
+  var bot_message = document.getElementById("bot-message");
+  var typewriter = new Typewriter(bot_message, {
+    loop: false,
+    cursor: "",
+  });
+
+  typewriter.deleteAll().typeString(str).start();
+}
--- basiclogic.js
+++ basiclogic.js
@@ -1,88 +1,83 @@
 function enterkey() {
-    if (window.event.keyCode == 13) {
-         // 엔터키가 눌렸을 때 실행할 내용
-         ordered();
-    }
+  if (window.event.keyCode == 13) {
+    // 엔터키가 눌렸을 때 실행할 내용
+    ordered();
+  }
 }
 
-function ordered(){
-    var input_message = document.getElementById('input-message');
-    setBotImage();
+function ordered() {
+  var input_message = document.getElementById("input-message");
+  setBotImage();
 
-    if ((input_message.value).indexOf('이름') > -1){
-        setBotMessage('내 이름은 아리야 >.<');
-        setBotImage('happy');
-    }
-    else if ((input_message.value).indexOf('안녕') > -1){
-        setBotMessage('반가워!');
-    }
-    else if ((input_message.value).indexOf('눈부셔') > -1){
-        setBotMessage('앗! 불 꺼줄게~!');
-        brightness('OFF');
-    }
-    else if ((input_message.value).indexOf('어두워') > -1){
-        setBotMessage('그래?! 불 켜줄게~!');
-        brightness('ON');
-    }
-    else if ((input_message.value).indexOf('춤') > -1){
-        setBotMessage('난 우울할 땐 힙합을 춰!');
-        setBotImage('dance');
-        DanceAnimate();
-    }
-    else if ((input_message.value.indexOf('멋져') > -1)){
-        setBotMessage('헤헤 고마워~~ 🖤');
-        setBotImage('happy');
-    }
-    else if ((input_message.value.indexOf('저리가') > -1)){
-        setBotMessage('끙,, 알겠어,,');
-        MoveAnimate();
-        setBotImage('sad');
-    }
-    else if ((input_message.value.indexOf('돌아와') > -1)){
-        setBotMessage('나 불렀어 ?!');
-        BackAnimate();
-        setBotImage('happy');
-    }
-    else{
-        setBotMessage('무슨 말인지 모르겠어 ㅠㅡㅠ');
-        setBotImage('sad');
-        NodAnimate();
-    }
+  if (input_message.value.indexOf("이름") > -1) {
+    typing("내 이름은 아리야 >.<");
+    Animate("bounce");
+    setBotImage("happy");
+  } else if (input_message.value.indexOf("안녕") > -1) {
+    typing("반가워!");
+    Animate("tada");
+  } else if (input_message.value.indexOf("눈부셔") > -1) {
+    typing("앗! 불 꺼줄게~!");
+    Animate("jello");
+    brightness("OFF");
+  } else if (input_message.value.indexOf("어두워") > -1) {
+    typing("그래?! 불 켜줄게~!");
+    Animate("jello");
+    brightness("ON");
+  } else if (input_message.value.indexOf("춤") > -1) {
+    typing("난 우울할 땐 힙합을 춰!");
+    setBotImage("dance");
+    DanceAnimate();
+  } else if (input_message.value.indexOf("멋져") > -1) {
+    typing("헤헤 고마워~~ 🖤");
+    Animate("swing");
+    setBotImage("happy");
+  } else if (input_message.value.indexOf("저리가") > -1) {
+    typing("끙,, 알겠어,,");
+    MoveAnimate();
+    setBotImage("sad");
+  } else if (input_message.value.indexOf("돌아와") > -1) {
+    typing("나 불렀어 ?!");
+    BackAnimate();
+    setBotImage("happy");
+  } 
+  else {
+    typing("무슨 말인지 모르겠어 ㅠㅡㅠ");
+    setBotImage("sad");
+    Animate("shakeX");
+  }
 
-    input_message.innerText = "";
-    typing();
+  input_message.innerText = "";
 }
 
-function setBotMessage(msg){
-    var bot_message = document.getElementById('bot-message');
-    bot_message.innerText = msg;
+// function setBotMessage(msg){
+//     var bot_message = document.getElementById('bot-message');
+//     bot_message.innerText = msg;
+// }
+
+function brightness(order) {
+  var background = document.getElementsByTagName("body")[0];
+  var input_message = document.getElementById("input-message");
+  var bot_message = document.getElementById("bot-message");
+
+  if (order === "OFF") {
+    background.style.backgroundColor = "#000";
+    input_message.style.backgroundColor = "#323232";
+    input_message.style.color = "#fff";
+    bot_message.style.color = "#fff";
+  } else {
+    background.style.backgroundColor = "#fff";
+    input_message.style.backgroundColor = "#fff";
+    input_message.style.color = "#000";
+    bot_message.style.color = "#000";
+  }
 }
 
-function brightness(order){
-    var background = document.getElementsByTagName('body')[0];
-    var input_message = document.getElementById('input-message');
-    var bot_message = document.getElementById('bot-message');
-
-    if (order === 'OFF'){
-        background.style.backgroundColor = '#000';
-        input_message.style.backgroundColor = '#323232';
-        input_message.style.color = '#fff';
-        bot_message.style.color = '#fff'
-    }
-    else{
-        background.style.backgroundColor = '#fff';
-        input_message.style.backgroundColor = '#fff';
-        input_message.style.color = '#000';
-        bot_message.style.color = '#000'
-    }
-}
-
-function setBotImage(name){
-    var target = document.getElementById('bot-image');
-    if (name == null){
-        target.src = './bot-image.png'; 
-    }
-    else{
-        target.src = './bot-image-' + name + '.png'; 
-    }
+function setBotImage(name) {
+  var target = document.getElementById("bot-image");
+  if (name == null) {
+    target.src = "./bot-image.png";
+  } else {
+    target.src = "./bot-image-" + name + ".png";
+  }
 }
